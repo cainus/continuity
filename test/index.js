116209c36d611d8cbd91e6b62b83cf16815aa65a
Gregg Caines <gregg@caines.ca>
2014-11-29 19:08:19 -0800

    run special travis-specific tests on travis.

--- test/index.js
+++ test/index.js
@@ -3,16 +3,32 @@ var getConfiguration = require("../index");
 
 describe("index", function(){
 
-  it ("can get local git info", function(done){
-    getConfiguration(function(err, config){
-      failOnError(err);
-      expect(config.commitId).
-            to.match(/[0-9a-f]{40}/);
-      expect(config.service).to.eql('localGit');
-      expect(config.buildId).to.eql('NONE');
-      expect(config.branch).to.eql('master');
-      done();
+  if (process.env.NODE_ENV == 'test'){
+    it ("can get local git info", function(done){
+      getConfiguration(function(err, config){
+        failOnError(err);
+        expect(config.commitId).
+              to.match(/[0-9a-f]{40}/);
+        expect(config.service).to.eql('localGit');
+        expect(config.buildId).to.eql('NONE');
+        expect(config.branch).to.eql('master');
+        done();
+      });
     });
-  });
+  }
+  if (process.env.NODE_ENV == 'test-travis'){
+    it ("can get travis info", function(done){
+      getConfiguration(function(err, config){
+        failOnError(err);
+        expect(config.commitId).
+              to.match(/[0-9a-f]{40}/);
+        expect(config.service).to.eql('travis');
+        expect(config.buildId).to.eql('NONE');
+        expect(config.branch).to.eql('master');
+        done();
+      });
+    });
+    
+  }
 
 });
